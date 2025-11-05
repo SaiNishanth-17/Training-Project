62ce992c3246e96e350b3664cd72ace1c2c036f9
Thandra <2438835@cognizant.com>
2025-11-05 18:29:08 +0530

    Admin dashboard Changes from topics and sub topics to subjects

--- src/app/Studentdashboard-component/availableexams-component/availableexams-component.ts
+++ src/app/Studentdashboard-component/availableexams-component/availableexams-component.ts
@@ -1,36 +1,30 @@
-
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StudentServices } from '../../Services/AvailableExamService';
 import { ExamTopicService } from '../../Services/exam-topic-service';
-import { Topics } from '../../exams-admin-components/topics/topics';
-
+// import { Topics } from '../../exams-admin-components/topics/topics';
 @Component({
   selector: 'app-available-exams',
-  imports:[CommonModule],
+  imports: [CommonModule],
   templateUrl: './availableexams-component.html',
-  styleUrls: ['./availableexams-component.css']
+  styleUrls: ['./availableexams-component.css'],
 })
 export class AvailableexamsComponent {
-
- 
-available: any[] = [];
+  available: any[] = [];
   active: any;
 
-  constructor(private availableExams: StudentServices,
-    private examtopicservice:ExamTopicService
+  constructor(
+    private availableExams: StudentServices,
+    private examtopicservice: ExamTopicService
   ) {}
 
   ngOnInit() {
-    this.available = this.examtopicservice.exams.filter(exam=>exam.isActive);
-    
+    this.available = this.examtopicservice.exams.filter((exam) => exam.isActive);
+
     // console.log(this.available);
   }
-  
 
   // AvailableExams() {
   //   this.available = this.availableExams.displayAvailableExams();
   // }
-  
-  }
-
+}
